78e67dc3d1092e36df7c4a022bf297fb51e5f901
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2021-02-15 11:49:01 +0900

    add function_exists()

--- iyo-scratch-blocks.php
+++ iyo-scratch-blocks.php
@@ -3,39 +3,47 @@
 Plugin Name: Iyo Scratch Blocks
 Plugin URI: https://coderdojo-iyo.com/
 Description: Visualize Scratch Code
-Version: 1.0
+Version: 1.0.1
 Author: Kazuaki Ueda
 Author URI: https://www.netplan.co.jp/
 License: GPL2
 */
 
-function add_iyo_scratch_blocks() {
-  wp_enqueue_script(
-    'scratchblocks',
-    'https://scratchblocks.github.io/js/scratchblocks-v3.5-min.js',
-    array(),
-    '3.5'
-  );
-  wp_enqueue_script(
-    'scratchblocks-translations',
-    'https://scratchblocks.github.io/js/translations-all-v3.5.js',
-    array(),
-    '3.5'
-  );
-}
-add_action('wp_enqueue_scripts', 'add_iyo_scratch_blocks');
+if ( !function_exists( 'iyo_scratch_blocks' ) ) {
 
-function add_iyo_scratch_render() {
-  ?>
-  <script>
-    scratchblocks.renderMatching(
-      '.scratchblocks',
-      {
-        languages: ["en", "ja"],
-        style: "scratch3"
-      }
+  function iyo_scratch_blocks() {
+    wp_enqueue_script(
+      'scratchblocks',
+      'https://scratchblocks.github.io/js/scratchblocks-v3.5-min.js',
+      array(),
+      '3.5'
     );
-  </script>
-  <?php
+    wp_enqueue_script(
+      'scratchblocks-translations',
+      'https://scratchblocks.github.io/js/translations-all-v3.5.js',
+      array(),
+      '3.5'
+    );
+  }
+  add_action('wp_enqueue_scripts', 'iyo_scratch_blocks');
+
 }
-add_action('wp_footer', 'add_iyo_scratch_render');
+
+if ( !function_exists( 'iyo_scratch_render' ) ) {
+
+  function iyo_scratch_render() {
+    ?>
+    <script>
+      scratchblocks.renderMatching(
+        '.scratchblocks',
+        {
+          languages: ["en", "ja"],
+          style: "scratch3"
+        }
+      );
+    </script>
+    <?php
+  }
+  add_action('wp_footer', 'iyo_scratch_render');
+
+}
